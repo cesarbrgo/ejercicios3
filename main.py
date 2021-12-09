1eb29ef734ee1dce410234bd2c8f0061090e111e
cesarbrgo <cbravo@institutmvm.cat>
2021-12-09 12:46:41 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    parells = [x for x in range(0, 31, 2)]
+    print(parells)
+
+
+if __name__ == '__main__':
+    main()
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
